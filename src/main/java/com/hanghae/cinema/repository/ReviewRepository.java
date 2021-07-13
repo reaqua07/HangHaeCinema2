f17b08a8ca6a548328feacd8dac068a6244a6371
swcide <swcide@gmail.com>
2021-07-13 19:36:22 +0900

    '통합본'

--- src/main/java/com/hanghae/cinema/repository/ReviewRepository.java
+++ src/main/java/com/hanghae/cinema/repository/ReviewRepository.java
@@ -5,11 +5,11 @@ import com.hanghae.cinema.model.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface ReviewRepository extends JpaRepository<Review,Long> {
     Page<Review> findReviewByMoviecodeOrderByCreatedAtDesc(String moviecode, Pageable pageable);
 
+    Review findReviewById(Long review_id);
 }
 
--- src/main/java/com/hanghae/cinema/util/MovieCrawling.java
+++ src/main/java/com/hanghae/cinema/util/MovieCrawling.java
@@ -2,13 +2,11 @@ package com.hanghae.cinema.util;
 
 
 import com.hanghae.cinema.dto.CrawlingDto;
-
 import com.hanghae.cinema.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -20,7 +18,7 @@ import java.util.stream.Collectors;
 public class MovieCrawling {
     private final MovieService movieService;
 
-
+    // 서버 실행과 동시에 크롤링 하겠다.
     @PostConstruct
     public List<CrawlingDto> movieListcrawling() throws Exception{
         String url = "https://movie.naver.com/movie/running/current.nhn";
@@ -29,15 +27,15 @@ public class MovieCrawling {
 
         return movieService.saveMovies(toList(element));
     }
-
     public List<CrawlingDto> toList(Elements element) {
         return element
                 .stream()
                 .map(el->new CrawlingDto(
                         el.select(".tit>a").text(),
-                        el.select(".star_t1>.num").text(),
+                        el.select(" dd.star > dl.info_star > dd > div > a > span.num").text(),
                         el.select(".tit>a").attr("href").split("code="),
                         "https://movie.naver.com/movie/bi/mi/photoViewPopup.nhn?movieCode="
                 )).collect(Collectors.toList());
+
     }
 }
